d9cd32e2bd9986fcae4cb606bdb25e64bf4b6003
Joseph Denison Cruz <61396938+denisiong@users.noreply.github.com>
2022-02-02 23:22:41 +0800

    Add files via upload

---
+++ Create&sampledata/sampledata.sql
@@ -0,0 +1,15 @@
+---------- INSERT INTO ADMIN---------- 
+
+insert into admins(email, password)  values('denden@gmail.com', 'Atos123%');
+insert into admins(email, password)  values('migs@gmail.com', 'Migs123');
+insert into admins(email, password)  values('timmy@gmail.com', 'timstims');
+
+----- INSERT INTO Category--------
+select * from categories;
+
+INSERT INTO categories (category_name) VALUES ('Electric Guitar');
+
+
+
+----------- INSERT INTO PRODUCTS--------
+SELECT * FROM PRODUCTS;
